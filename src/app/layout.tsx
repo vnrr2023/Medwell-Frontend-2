7d8309f46f8a2aa4132465b547db00de5bbd3064
Rehan Sayyed <125148030+rsayyed591@users.noreply.github.com>
2025-01-28 12:24:02 +0530

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next"
 import "./globals.css"
 import { Suspense, lazy } from "react"
 import { Inter } from "next/font/google"
-import { BackgroundBeams } from "@/components/ui/background-beams"
+// import { BackgroundBeams } from "@/components/ui/background-beams"
 import { QueryProvider } from "@/components/QueryProvider"
 
 const Navbar = lazy(() => import("@/components/Navbar"))
@@ -25,7 +25,7 @@ export default function RootLayout({
       <body className={inter.className}>
         <QueryProvider>
           <div className="min-h-screen bg-[#F5F5F5] text-[#212121] relative">
-            <BackgroundBeams className="fixed inset-0 z-0 pointer-events-none" />
+           // <BackgroundBeams className="fixed inset-0 z-0 pointer-events-none" />
             <div className="relative z-10">
               <Suspense fallback={<div>Loading...</div>}>
                 <Navbar />
