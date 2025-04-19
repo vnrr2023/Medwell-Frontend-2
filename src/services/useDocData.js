265db853a198c5b53dc61757184e3212429e542c
Nishikant00 <githubnsr@gmail.com>
2025-04-19 21:14:16 +0530

    apis

--- src/services/useDocData.js
+++ src/services/useDocData.js
@@ -5,7 +5,7 @@ import axios from "axios"
 import { ngrok_url2, ngrok_url, ngrok_url_appointment } from "./api"
 import { Token as token } from "./api"
 const API_URL = ngrok_url_appointment+"/"
-const API_URL2 = ngrok_url+"/"
+const API_URL2 = ngrok_url_main+"/"
 
 const useDocData = () => {
 
